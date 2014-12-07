455f8100ea1aaee2b1e6cd7fce18b998a5e6a00e
albertrdixon <albert@timelinelabs.com>
2014-12-07 01:00:06 -0800

    Changing the default peer port and some paths

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -10,7 +10,7 @@ run_cmd() {
 : ${USERNAME:="client"}
 : ${PASSWORD:=""}
 : ${RPC_PORT:="9091"}
-: ${PEER_PORT:="51413"}
+: ${PEER_PORT:="51234"}
 
 test -d $DOWNLOAD_DIR || mkdir -p $DOWNLOAD_DIR
 test -d $WATCH_DIR || mkdir -p $WATCH_DIR
@@ -21,8 +21,9 @@ else
   EXTRA_OPTS=""
 fi
 
-sed -i "s|__password__|$PASSWORD|" /root/.config/transmission-daemon/
-sed -i "s|__username__|$USERNAME|" /root/.config/transmission-daemon/
-sed -i "s|__rpc_port__|$RPC_PORT|" /root/.config/transmission-daemon/
-sed -i "s|__peer_port__|$PEER_PORT|" /root/.config/transmission-daemon/
+test -f "$TRANSMISSION_HOME/settings.json" || cp /settings.json "$TRANSMISSION_HOME/"
+sed -i "s|__password__|$PASSWORD|" "$TRANSMISSION_HOME/settings.json"
+sed -i "s|__username__|$USERNAME|" "$TRANSMISSION_HOME/settings.json"
+sed -i "s|__rpc_port__|$RPC_PORT|" "$TRANSMISSION_HOME/settings.json"
+sed -i "s|__peer_port__|$PEER_PORT|" "$TRANSMISSION_HOME/settings.json"
 run_cmd "transmission-daemon -f -e /dev/stdout $EXTRA_OPTS"
